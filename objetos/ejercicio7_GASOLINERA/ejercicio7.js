b6450b646ce189ea39ceef2a0cbd4e11281de69c
carmenrosacoder <carmenrosa.araceli.arias.arias@gmail.com>
2016-05-06 21:43:54 -0500

    obejtos

--- objetos/ejercicio7_GASOLINERA/ejercicio7.js
+++ objetos/ejercicio7_GASOLINERA/ejercicio7.js
@@ -3,10 +3,15 @@ var gasolinera={
     litros: 200,
     precio:7,
     litrosRetira:null,
+
     dispensa: function(){
 if(this.litrosRetira<=this.litros){
     this.litros -= this.litrosRetira;
-    return this.litros;
+    return this.precio*this.litrosRetira;
+}
+else if(this.litrosRetira>=this.litros){
+  return this.litros+"litros";
+
 }
 else{
     return "El numero de litros disponible es:"+this.litros;
@@ -16,5 +21,6 @@ else{
 alert('Dispensador con '+gasolinera.litros+' litros\n Precio:'+gasolinera.precio+'soles');
 do{
 gasolinera.litrosRetira=parseInt(prompt('¿Cuantos litros retirará?: '));
-alert('Debe pagar'+gasolinera.precio*gasolinera.litrosRetira+'soles\n'+'Solo quedan'+gasolinera.dispensa()+'litros');
-}while(gasolinera.litrosRetira<=gasolinera.litros);
+alert('Debe pagar'+gasolinera.dispensa()+'soles\n'+'Solo quedan'+gasolinera.litros+'litros');
+}while(gasolinera.litros>0);
+alert('Se acabo la gasolina');
